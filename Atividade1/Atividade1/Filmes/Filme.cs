ab0794c9db72be28d169130a55aeed7e28b51996
TiagoDesc <nobretiago96@gmail.com>
2024-03-23 23:45:32 -0300

    ex1

---
+++ Atividade1/Atividade1/Filmes/Filme.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade1.Filmes
+{
+    internal class Filme
+    {
+        private List <string> Elenco { get; set; }
+        private string Titulo { get; set; }
+        private int Duracao { get; set; }
+
+        public Filme(string titulo, int duracao, List<string>? elenco)
+        {
+            if(elenco == null)
+            {
+                Elenco = new List<string>();
+            }
+            else
+            {
+                Elenco = elenco;
+            }
+
+            Titulo = titulo;
+            Duracao = duracao;
+        }
+
+        public void AdicionarElenco(string ator)
+        {
+            Elenco.Add(ator);
+            Console.WriteLine($"{ator} foi adicionado/a ao elenco!");
+        }
+
+        public void ListarElenco()
+        {
+
+            if(Elenco.Count == 0)
+            {
+                Console.WriteLine("O elenco está vazio.");
+            }
+            else
+            {
+                Console.WriteLine("Elenco:");
+                foreach (var ator in Elenco) {
+                    Console.WriteLine(ator);
+
+                }
+            }
+            
+        }
+    }
+}
