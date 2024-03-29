7e9cfbfa7cec699e3c266337035e97130d46434c
TiagoDesc <nobretiago96@gmail.com>
2024-03-29 01:15:08 -0300

    a mimir. It's 01:14am

---
+++ Atividade2/Atividade2/Classes/PetShop/Consulta.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade2.Classes.PetShop
+{
+
+    //animal
+    //donoAnimal
+    //Veterianrio
+    //dataconsulta
+    internal class Consulta
+    {
+
+        public Pet Animal { get; set; }
+        public Dono DonoAnimal { get; set; }
+        public Medico Veterinario {  get; set; }
+
+        public string DataConsulta { get; set; }
+
+        public Consulta(Pet animal, Dono donoAnimal, Medico veterinario, string dataConsulta)
+        {
+            Animal = animal;
+            DonoAnimal = donoAnimal;
+            Veterinario = veterinario;
+            DataConsulta = dataConsulta;
+        }
+    }
+}
---
+++ Atividade2/Atividade2/Classes/PetShop/Medico.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade2.Classes.PetShop
+{
+    internal class Medico
+    {
+        public string Nome { get; set; }
+        public string Especialidade { get; set; }
+
+        public Medico(string nome, string especialidade)
+        {
+            Nome = nome;
+            Especialidade = especialidade;
+        }
+    }
+}
