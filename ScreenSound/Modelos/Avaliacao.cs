2446d31f6f34fd860d061a2a5e99c25d1ae8fa44
TiagoDesc <nobretiago96@gmail.com>
2024-03-29 00:07:50 -0300

    att

--- ScreenSound/Modelos/Avaliacao.cs
+++ ScreenSound/Modelos/Avaliacao.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+namespace ScreenSound.Modelos;
 
-namespace ScreenSound.Modelos
+internal class Avaliacao
 {
-    internal class Avaliacao
-    {
-    }
 }
