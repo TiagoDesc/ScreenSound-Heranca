7934eac21c51fab311b2a284be6f12886f6b508f
TiagoDesc <nobretiago96@gmail.com>
2024-04-01 23:57:38 -0300

    new class

--- Atividade2/Atividade2/Classes/Oficina/Veiculo.cs
+++ Atividade2/Atividade2/Classes/Oficina/Veiculo.cs
@@ -8,5 +8,17 @@ namespace Atividade2.Classes.Oficina
 {
     internal class Veiculo
     {
+        public string Marca {  get; set; }
+        public string Modelo { get; set; }
+        public int Ano {  get; set; }
+        public string Placa { get; set; }
+
+        public Veiculo(string marca, string modelo, int ano, string placa)
+        {
+            Marca = marca;
+            Modelo = modelo;
+            Ano = ano;
+            Placa = placa;
+        }
     }
 }
