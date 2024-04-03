36a9a50585fa84002d09fe7b06e952b2acf6c240
TiagoDesc <nobretiago96@gmail.com>
2024-04-02 23:36:43 -0300

    program

---
+++ Atividade2/Atividade2/Classes/Oficina/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade2.Classes.Oficina
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Veiculo meuCarro = new Veiculo("Chevrolet", "Cruze", 2020, "ABC1234");
+            Cliente cliente = new Cliente("Carlos", "987654321");
+            Mecanico mecanico = new Mecanico("Mário", "Mecânica Geral");
+            Oficina oficina = new Oficina();
+
+            oficina.AgendamentoDeServico(meuCarro, cliente, mecanico, "2023-12-18");
+            oficina.RealizarServico(meuCarro, mecanico);
+        }
+
+    }
+}
