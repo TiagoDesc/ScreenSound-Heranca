30143f00ea090488828760a27c2ebfd0c57927e8
TiagoDesc <nobretiago96@gmail.com>
2024-04-02 23:53:17 -0300

    delete

--- Atividade2/Atividade2/Classes/Oficina/Program.cs
+++ Atividade2/Atividade2/Classes/Oficina/Program.cs
@@ -1,23 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Atividade2.Classes.Oficina;
 
-namespace Atividade2.Classes.Oficina
+internal class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main()
-        {
-            Veiculo meuCarro = new Veiculo("Chevrolet", "Cruze", 2020, "ABC1234");
-            Cliente cliente = new Cliente("Carlos", "987654321");
-            Mecanico mecanico = new Mecanico("Mário", "Mecânica Geral");
-            Oficina oficina = new Oficina();
+        Veiculo meuCarro = new Veiculo("Chevrolet", "Cruze", 2020, "ABC1234");
+        Cliente cliente = new Cliente("Carlos", "987654321");
+        Mecanico mecanico = new Mecanico("Mário", "Mecânica Geral");
+        Oficina oficina = new Oficina();
 
-            oficina.AgendamentoDeServico(meuCarro, cliente, mecanico, "2023-12-18");
-            oficina.RealizarServico(meuCarro, mecanico);
-        }
-
+        oficina.AgendamentoDeServico(meuCarro, cliente, mecanico, "2023-12-18");
+        oficina.RealizarServico(meuCarro, mecanico);
     }
+
 }
