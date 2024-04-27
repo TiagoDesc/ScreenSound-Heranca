caaa6cbb2adb7578e4936b2223b1cc4fb4fd3901
TiagoDesc <nobretiago96@gmail.com>
2024-04-26 23:52:39 -0300

    short

--- ScreenSound/Menus/MenuAvaliarBanda.cs
+++ ScreenSound/Menus/MenuAvaliarBanda.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+namespace ScreenSound.Menus;
 
-namespace ScreenSound.Menus
+internal class MenuAvaliarBanda
 {
-    internal class MenuAvaliarBanda
-    {
-    }
 }
