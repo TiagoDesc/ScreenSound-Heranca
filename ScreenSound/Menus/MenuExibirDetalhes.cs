f3d1bb42cfb3b1a6f9d9919e2dd2972efce53921
TiagoDesc <nobretiago96@gmail.com>
2024-04-27 00:03:57 -0300

    new

--- ScreenSound/Menus/MenuExibirDetalhes.cs
+++ ScreenSound/Menus/MenuExibirDetalhes.cs
@@ -2,17 +2,10 @@
 
 namespace ScreenSound.Menus;
 
-internal class MenuExibirDetalhes
+internal class MenuExibirDetalhes : Menu
 {
 
-    void ExibirTituloDaOpcao(string titulo)
-    {
-        int quantidadeDeLetras = titulo.Length;
-        string asteriscos = string.Empty.PadLeft(quantidadeDeLetras, '*');
-        Console.WriteLine(asteriscos);
-        Console.WriteLine(titulo);
-        Console.WriteLine(asteriscos + "\n");
-    }
+    
 
     public void Executar(Dictionary<string, Banda> bandasRegistradas)
     {
