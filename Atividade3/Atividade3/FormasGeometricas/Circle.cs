9675992eaeebe2203aff2a181b07436a69406558
TiagoDesc <nobretiago96@gmail.com>
2024-05-08 00:27:31 -0300

    radius

---
+++ Atividade3/Atividade3/FormasGeometricas/Circle.cs
@@ -0,0 +1,18 @@
+﻿using System.Transactions;
+
+namespace Atividade3.FormasGeometricas;
+
+internal class Circle : Shape
+{
+    public double Radius { get; set; }
+
+    public override double AreaCalculator()
+    {
+        return Math.PI * Radius * Radius;
+    }
+
+    public override double PerimeterCalculator()
+    {
+        return 2 * Math.PI * Radius;
+    }
+}
