466589e127002c754941345f246735218dae1e70
TiagoDesc <nobretiago96@gmail.com>
2024-05-08 00:24:16 -0300

    new

---
+++ Atividade3/Atividade3/FormasGeometricas/Square.cs
@@ -0,0 +1,15 @@
+﻿namespace Atividade3.FormasGeometricas;
+
+internal class Square : Shape
+{
+    public double Side {  get; set; }
+    public override double AreaCalculator()
+    {
+        return Side * Side;
+    }
+
+    public override double PerimeterCalculator()
+    {
+        return 4 * Side;
+    }
+}
