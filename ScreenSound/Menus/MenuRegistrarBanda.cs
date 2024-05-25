2c95043c6a620b972a2c53075539033cbb53f8fd
TiagoDesc <nobretiago96@gmail.com>
2024-05-25 02:50:48 -0300

    foi

--- ScreenSound/Menus/MenuRegistrarBanda.cs
+++ ScreenSound/Menus/MenuRegistrarBanda.cs
@@ -1,34 +1,28 @@
-﻿using OpenAI_API;
-using ScreenSound.Modelos;
+﻿using ScreenSound.Modelos;
+using OpenAI_API;
 
-namespace ScreenSound.Menus
+namespace ScreenSound.Menus;
+
+internal class MenuRegistrarBanda : Menu
 {
-    internal class MenuRegistrarBanda : Menu
+    public override void Executar(Dictionary<string, Banda> bandasRegistradas)
     {
-        public async override void Executar(Dictionary<string, Banda> bandasRegistradas)
-        {
-            base.Executar(bandasRegistradas);
-            ExibirTituloDaOpcao("Registro das bandas");
-            Console.Write("Digite o nome da banda que deseja registrar: ");
-            string nomeDaBanda = Console.ReadLine()!;
-            Banda banda = new Banda(nomeDaBanda);
-            bandasRegistradas.Add(nomeDaBanda, banda);
+        base.Executar(bandasRegistradas);
+        ExibirTituloDaOpcao("Registro das bandas");
+        Console.Write("Digite o nome da banda que deseja registrar: ");
+        string nomeDaBanda = Console.ReadLine()!;
+        Banda banda = new Banda(nomeDaBanda);
+        bandasRegistradas.Add(nomeDaBanda, banda);
 
-            //string apiKey = Environment.GetEnvironmentVariable("OPENAI_API_KEY");
-            //var client = new OpenAIAPI(apiKey);
+        var client = new OpenAIAPI("<SUA API KEY AQUI>");
+        var chat = client.Chat.CreateConversation();
+        chat.AppendSystemMessage($"Resuma a banda {nomeDaBanda} em 1 parágrafo. Adote um estilo informal");
+        var resposta = chat.GetResponseFromChatbotAsync().GetAwaiter().GetResult();
+        banda.Resumo = resposta;
 
-            //var chat = client.Chat.CreateConversation();
-
-            //chat.AppendSystemMessage($"Resuma a banda {nomeDaBanda} em uma linguagem formal e bem descritiva.");
-            
-            //string resposta = chat.GetResponseFromChatbotAsync().GetAwaiter().GetResult();
-            //banda.Resumo = resposta;
-            //Console.WriteLine(resposta);
-
-            Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
-            Console.WriteLine("Digite uma tecla para voltar ao menu principal");
-            Console.ReadKey();
-            Console.Clear();
-        }
+        Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
+        Console.WriteLine("Digite uma tecla para voltar ao menu principal");
+        Console.ReadKey();
+        Console.Clear();
     }
 }
