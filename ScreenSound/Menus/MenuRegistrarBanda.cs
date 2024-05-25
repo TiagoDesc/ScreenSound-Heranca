ad52cdfe98996e0c1610aef904a5b5fb421b441c
Tiago Nobre <132406448+TiagoDesc@users.noreply.github.com>
2024-05-25 03:01:22 -0300

    Update MenuRegistrarBanda.cs

--- ScreenSound/Menus/MenuRegistrarBanda.cs
+++ ScreenSound/Menus/MenuRegistrarBanda.cs
@@ -1,10 +1,11 @@
-﻿using ScreenSound.Modelos;
+using OpenAI_API;
+using ScreenSound.Modelos;
 
 namespace ScreenSound.Menus;
 
 internal class MenuRegistrarBanda : Menu
 {
-    public override void Executar(Dictionary<string, Banda> bandasRegistradas)
+    public async override void Executar(Dictionary<string, Banda> bandasRegistradas)
     {
         base.Executar(bandasRegistradas);
         ExibirTituloDaOpcao("Registro das bandas");
@@ -12,8 +13,21 @@ internal class MenuRegistrarBanda : Menu
         string nomeDaBanda = Console.ReadLine()!;
         Banda banda = new Banda(nomeDaBanda);
         bandasRegistradas.Add(nomeDaBanda, banda);
+
+        var client = new OpenAIAPI("sk-proj-PuVA7QjfSHgNHwF9ahvZT3BlbkFJOikuUunIe7pzdrjsqwVk");
+
+        var chat = client.Chat.CreateConversation();
+
+        chat.AppendSystemMessage($"Resuma a banda {nomeDaBanda} em uma linguagem formal.");
+
+        string resposta = chat.GetResponseFromChatbotAsync().GetAwaiter().GetResult();
+        banda.Resumo = resposta;
+        Console.WriteLine(resposta);
+
         Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
-        Thread.Sleep(4000);
+        Console.WriteLine("Digite uma tecla para voltar ao menu principal");
+        Console.ReadKey();
         Console.Clear();
+
     }
 }
